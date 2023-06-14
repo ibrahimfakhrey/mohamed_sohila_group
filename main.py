5967b3a3fbc0e69d309f20e908cd4618c71620bd
ibrahimfakhry <Ibrahim fakhrey@github.com>
2023-06-14 12:33:03 +0300

    session 2

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 app=Flask(__name__)
 @app.route("/")
 def start():
+    return render_template("index.html")
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+    if request.method=="POST":
+        name=request.form.get("user")
+        password=request.form.get("password")
+        if name =="abdullah" and password=="1234":
+            return "my secret is that programming is interesting "
+        else:
+            return "you are not abdullah try again "
+
     return render_template("login.html")
 
-@app.route("/mohamed")
-def mohamed():
-    return "sohila"
-@app.route("/mohaned")
-def m():
-    return"mohanad"
+
+
+
+
 if __name__=="__main__":
     app.run(debug=True)

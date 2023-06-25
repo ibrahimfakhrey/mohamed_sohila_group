d47bd33533034e234d56cf10da5d4892fa787bf2
Admin <ibrahimfakhreyams@gmail.com>
2023-06-25 13:07:41 +0300

    add

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request, redirect
 import requests
 import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 lat = "30.357519"
 lon = "31.204611"
@@ -19,6 +24,23 @@ users={
     "ali":"1234",
     "omar":"1122"
 }
+app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+with app.app_context():
+    class User(UserMixin, db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        phone = db.Column(db.String(100), unique=True)
+        password = db.Column(db.String(100))
+        name = db.Column(db.String(1000))
+    db.create_all()
+class MyModelView(ModelView):
+    def is_accessible(self):
+            return False
+
+admin = Admin(app)
+admin.add_view(MyModelView(User, db.session))
 @app.route("/")
 def start():
     return render_template("index.html")
